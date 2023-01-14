1eb049d3955d1446e49c629f7903e1079f01bc28
jgsam <jgsam2009@gmail.com>
2023-01-13 20:01:51 -0500

    Proyecto AW

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,10 @@
-/*
-
-TemplateMo 559 Zay Shop
-
-https://templatemo.com/tm-559-zay-shop
-
-*/
-
 'use strict';
 $(document).ready(function() {
 
     // Accordion
-    var all_panels = $('.templatemo-accordion > li > ul').hide();
+    var all_panels = $('.accordion > li > ul').hide();
 
-    $('.templatemo-accordion > li > a').click(function() {
+    $('.accordion > li > a').click(function() {
         console.log('Hello world!');
         var target =  $(this).next();
         if(!target.hasClass('active')){
@@ -21,9 +13,7 @@ $(document).ready(function() {
         }
       return false;
     });
-    // End accordion
-
-    // Product detail
+    
     $('.product-links-wap a').click(function(){
       var this_src = $(this).children('img').attr('src');
       $('#product-detail').attr('src',this_src);
@@ -52,6 +42,6 @@ $(document).ready(function() {
       $(this).addClass('btn-secondary');
       return false;
     });
-    // End roduct detail
+   
 
 });
